49afe88840c3d6dd432cb1ef8df1c6d755fc3b4b
hayden cordeiro <40896224+haydencordeiro@users.noreply.github.com>
2020-08-25 20:21:05 +0530

    5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function Delete(ele) {
   // var total=document.getElementsByClassName
-  var temp = document.getElementsByClassName(ele.id)[0];
-  var temp2 = document.getElementsByClassName(ele.id)[1];
+  var temp = document.getElementsByClassName(ele.id)[1];
+  var temp2 = document.getElementsByClassName(ele.id)[2];
   if (temp.className == "deletehidden " + ele.id) {
     temp.className = "deleteshow " + ele.id + " pop";
     temp2.className = "deleteshow " + ele.id;
@@ -21,4 +21,26 @@ function myFunction() {
     aside.className = "show";
     main.className = "hidden";
   }
+}
+
+function State(ele) {
+  var id = ele.parentElement.parentElement.children[0].children[0].id;
+  var parent = ele.parentElement.parentElement;
+
+  if (parent.className == "item notdone " + id) {
+    parent.className = "item done " + id;
+  } else {
+    parent.className = "item notdone " + id;
+  }
+}
+
+function removeElement(ele) {
+  // Removes an element from the document
+  var parent = ele.parentNode;
+  var gparent = parent.parentNode;
+  var id = parent.className.split(" ")[1];
+  var temp = document.getElementsByClassName(id)[0];
+  gparent.removeChild(temp);
+
+  gparent.removeChild(parent);
 }
