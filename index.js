2010cc6a03baba31f69d4451d64f60e61737cf51
hayden cordeiro <40896224+haydencordeiro@users.noreply.github.com>
2020-08-25 19:18:00 +0530

    4

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function Delete(ele) {
+  // var total=document.getElementsByClassName
   var temp = document.getElementsByClassName(ele.id)[0];
   var temp2 = document.getElementsByClassName(ele.id)[1];
   if (temp.className == "deletehidden " + ele.id) {
-    temp.className = "deleteshow " + ele.id;
+    temp.className = "deleteshow " + ele.id + " pop";
     temp2.className = "deleteshow " + ele.id;
   } else {
     temp.className = "deletehidden " + ele.id;
